51d9ced24818bc322ba1d5ab12e8a46c4016bbe8
Linu Ramachandran <linuram@gmail.com>
2019-04-06 14:03:08 -0400

    Initialize project

---
+++ models/index.js
@@ -0,0 +1,10 @@
+// Exporting an object containing all of our models
+const Article = require('./Article')
+const SavedArticle = require('./SavedArticle')
+const Note = require('./Note')
+
+module.exports = {
+  Article,
+  SavedArticle,
+  Note
+}
